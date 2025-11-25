5e5d38241e83d77bd861034d6a2e3c89a1d7ed97
damares24 <damares290619@gmail.com>
2025-11-25 09:34:30 -0300

    Convertendo tipos

---
+++ Aula_03/convertendo_tipos.py
@@ -0,0 +1,18 @@
+print(int(1.9))
+print(int('10'))
+print(float("10.10"))
+print(float(100))
+
+valor = 10
+valor_str = str(valor)
+#Imprimindo tipos
+#Tipo int
+print(type(valor))
+#Tipo str
+print(type(valor_str))
+
+#Imprimindo divisões 
+#Esta saída sai do tipo float
+print(100 / 2)
+#Esta saída sai do tipo int
+print(100 // 2)
---
+++ Aula_03/variaveis_constantes.py
@@ -0,0 +1,13 @@
+nome = 'Damares'
+idade = 28
+
+nome, idade = 'Emanoele',29
+
+print(nome, idade)
+
+limite_saque_diario = 1000
+
+BRAZILIAN_STATES = ['SP', 'RJ', 'SC', 'RS']
+
+
+print(BRAZILIAN_STATES)
